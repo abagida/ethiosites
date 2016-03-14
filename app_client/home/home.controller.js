4e60b0250c4ef4b78ff4654b6f1a4b55c6000752
Yisehak Ali <yishak21@gmail.com>
2016-03-14 08:51:53 +0300

    yis commit

--- app_client/home/home.controller.js
+++ app_client/home/home.controller.js
@@ -13,11 +13,11 @@
     var vm = this;
     console.log(window.location);
     vm.pageHeader = {
-      title: 'Loc8r',
-      strapline: 'Find places to work with wifi near you!'
+      title: 'Site-Locator',
+      strapline: 'Find sites near you!'
     };
     vm.sidebar = {
-      content: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for."
+      content: "Looking for information about ET network sites?You have come to the right place! Let the website help you find the place you're looking for."
     };
     vm.message = "Checking your location";
 
