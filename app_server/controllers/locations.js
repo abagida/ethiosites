5f4581e10eab3543d517be187f7bda30ea7a62b8
Yisehak Ali <yishak21@gmail.com>
2016-03-16 14:15:57 +0300

    yis

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -14,10 +14,10 @@ var _formatDistance = function (distance) {
   var numDistance, unit;
   if (distance && _isNumeric(distance)) {
     if (distance > 1) {
-      numDistance = parseFloat(distance).toFixed(1);
+      numDistance = parseFloat(distance/1000).toFixed(1);
       unit = 'km';
     } else {
-      numDistance = parseInt(distance*1000,10);
+      numDistance = parseInt(distance,10);
       unit = 'm';
     }
     return numDistance + unit;
