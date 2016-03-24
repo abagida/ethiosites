f8775e546c400414695a260f86a2ec99f6e998ae
Yisehak Ali <yishak21@gmail.com>
2016-03-24 10:27:02 +0300

    iman

--- app_client/home/home.controller.js
+++ app_client/home/home.controller.js
@@ -22,8 +22,8 @@
     vm.message = "Checking your location";
 
     vm.getData = function (position) {
-      var lat = position.coords.latitude,
-          lng = position.coords.longitude;
+      var lat = 7.6761,
+          lng = 36.8293;
       vm.message = "Searching for nearby places";
       loc8rData.locationByCoords(lat, lng)
         .success(function(data) {
