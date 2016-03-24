8ee76213b5bdbc55b3384b4f4cdfd557970453a6
Yisehak Ali <yishak21@gmail.com>
2016-03-24 12:02:59 +0300

    iman

--- app_client/common/services/geolocation.service.js
+++ app_client/common/services/geolocation.service.js
@@ -5,14 +5,7 @@
     .service('geolocation', geolocation);
 
   function geolocation () {
-    var getPosition = function (cbSuccess, cbError, cbNoGeo) {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(cbSuccess, cbError);
-      }
-      else {
-        cbNoGeo();
-      }
-    };
+    
     return {
       getPosition : getPosition
     };
