d146d6b4e6098924f800e50f9e2283c5738bddf0
Yisehak Ali <yishak21@gmail.com>
2016-05-11 19:21:24 +0300

    jil

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var passport = require('passport');
 require('./app_api/models/db');
 require('./app_api/config/passport');
 
-var routes = require('./app_server/routes/index');
+//var routes = require('./app_server/routes/index');
 var routesApi = require('./app_api/routes/index');
 
 var app = express();
